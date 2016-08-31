fae2cbf9ac544939a85926d3711caf38fca43a80
Vadim <vadimkov@gmail.com>
2016-08-31 15:13:58 -0400

    Added GalleryBase constructor phpdoc comments

--- docroot/modules/gallery/src/GalleryBase.php
+++ docroot/modules/gallery/src/GalleryBase.php
@@ -44,7 +44,18 @@ class GalleryBase extends PluginBase implements GalleryInterface, ContainerFacto
   protected $entityViewMode;
 
   /**
-   * {@inheritdoc}
+   * GalleryBase constructor.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Entity\EntityManagerInterface $entity_manager
+   *   The entity manager service.
+   * @param \Drupal\Core\Entity\Query\QueryFactory $entity_query
+   *   The entity query service.
    */
   public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityManagerInterface $entity_manager, QueryFactory $entity_query) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
